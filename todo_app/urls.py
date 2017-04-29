4857779e993db6a4dce020d2bcc89e3c093c2dd6
simon-japan <simon.holland@gmail.com>
2017-04-29 20:17:27 +0900

    working thru django tutorial - adapted to make todo app instead of a poll.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^projects/(?P<project_id>[0-9]+)/$', views.project),
+    url(r'^todo/(?P<todo_id>[0-9]+)/$', views.todo_detail)
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Project
+from .models import Todo
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the todo app index.")
+
+
+def project(request, project_id):
+    p = Project.objects.get(pk=project_id)
+    return HttpResponse(p.name)
+
+
+def todo_detail(request, todo_id):
+    return HttpResponse("You're looking at todo %s" % todo_id)
+
